3e284c3dd05b6ca5b28bc289ae52c7b3d0fe9d27
Tier5 LLC <akash@tier5.in>
2019-01-10 20:37:06 +0530

    New design implementation v1.3

--- resources/views/new_version/shared/sticky-note.blade.php
+++ resources/views/new_version/shared/sticky-note.blade.php
@@ -1,6 +1,6 @@
 <div class="stickyBox" id="stickyBoxWrap" style="display: none;">
     <div id="popup">
-        <img src="{{config('settings.LANDING-DOMAIN')}}/public/images/sticky_Logo.png">
+        <img src="{{config('settings.APPLICATION-DOMAIN')}}/public/images/sticky_Logo.png">
         <div class="stickyText">
         <p>You have unlocked 4 leads today.</p>
         <p>You can unlocked upto <span>50</span> leads per day.</p>
