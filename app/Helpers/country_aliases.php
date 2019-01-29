bf1525bc10918b07e4d30be2b28e00c3d9e7a013
Tier5 LLC <akash@tier5.in>
2019-01-29 16:50:03 +0530

    New design implementation v6.4

---
+++ app/Helpers/country_aliases.php
@@ -0,0 +1,22 @@
+<?php
+
+function custom_country_aliases() {
+    return [
+        'united states of america' => 'United States',
+        'u.s.a' => 'United States',
+        'u.s.a.' => 'United States',
+        'great britain' => 'United Kingdom',
+        'great britain (uk)' => 'United Kingdom',
+        'uk' => 'United Kingdom',
+        'de' => 'Germany',
+        'de (germany)' => 'Germany',
+        'australia (au)' => 'Australia',
+        'fr (france)' => 'France',
+        'france m?tropolitaine' => 'France',
+        'france mtropolitaine' => 'France',
+        'ae (united arab emirates)' => 'United Arab Emirates',
+        'united arab emirates (ae)' => 'United Arab Emirates',
+    ];
+}
+
+?>
