24ae37632e8a7589dc265ac85b9a1a72ba4f52cf
tier5 <work@tier5.us>
2019-01-30 20:16:16 +0530

    upgrad_ membership_page design v0.1

--- js/custom.js
+++ js/custom.js
@@ -7,28 +7,48 @@ $(document).ready(function(){
   $('.menuClose').click(function(){
     $('.bottomRight').removeClass('pull');
   });
+  // for vier more toggle
+  $(".viewMore1").click(function(){
+    $(".viewMorePanel1").toggle();
+  });
+  $(".viewMore2").click(function(){
+    $(".viewMorePanel2").toggle();
+  });
+  $(".viewMore3").click(function(){
+    $(".viewMorePanel3").toggle();
+  });
 
-  $('.fancybox').fancybox();
+// for sight panel  
+    if($(this).hasClass("open")){
+        $(this).removeClass("open");
+        $(".rightPanel").removeClass("open");
+        $(".mainBody").scrollTop(bodyScroll);
+        $(".leftPanel").css("opacity","1");
+    } else {
+        bodyScroll = $(".mainBody").scrollTop();
+        $(".mainBody").scrollTop(0);
+        $(this).addClass("open");
+        $(".rightPanel").addClass("open");
+        $(".leftPanel").css("opacity","0.2");
+    }
+});
+
+
+  // $('.fancybox').fancybox();
   
 
 //   $('#bannercaptionWrap').scroll(function() { 
 //     $('#banner_img_left').animate({top:$(this).scrollTop()},100,"linear");
 // })
 
-
- });
-
-
-
-
 // for sidepopup
-function PopUp(hideOrshow) {
-      if (hideOrshow == 'hide') document.getElementById('stickyBoxWrap').style.display = "none";
-      else document.getElementById('stickyBoxWrap').removeAttribute('style');
-      }
-      window.onload = function () {
-          setTimeout(function () {
-              PopUp('show' , '1000');
-          }, 2000);
-      }
+// function PopUp(hideOrshow) {
+//       if (hideOrshow == 'hide') document.getElementById('stickyBoxWrap').style.display = "none";
+//       else document.getElementById('stickyBoxWrap').removeAttribute('style');
+//       }
+//       window.onload = function () {
+//           setTimeout(function () {
+//               PopUp('show' , '1000');
+//           }, 2000);
+//       }
 
