a510e95b0162aa1b830cffce382f8f5c316427c1
Tier5 LLC <akash@tier5.in>
2019-02-25 15:34:50 +0530

    Subscription and registration module v1.1

---
+++ config/routeSettings.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'pendingSubscription' => [
+        'failedSubscription', 'failedSubscriptionPost','logout'
+    ],
+    'subscribedUserGroup' => [
+        'showMembershipPage', 'updateCardDetailsAndSubscribe', 'upgradeOrDowngradePlan','logout'
+    ]
+];
+?>
